f10d0adcbc31890d6f9c2b31d466fb016bc3f3bd
guilhermescr <guilhermechaves2005@hotmail.com>
2023-05-15 17:40:37 -0300

    feat: add sign in and sign up buttons in home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <h1>Início</h1>
+      <Link to="/signin" style={{ marginLeft: '15px' }}>
+        Entrar
+      </Link>
+      <Link to="/signup" style={{ marginLeft: '15px' }}>
+        Cadastrar-se
+      </Link>
     </>
   );
 }
